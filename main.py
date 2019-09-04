cce6267f27a2a8a8c4ed58243c42a8ee95ddda57
Skye Luna <skyeluna@users.noreply.github.com>
2019-09-04 12:11:20 +0100

    First Commit
    Added base Window of game + icon
    
    Fox Image not mine

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pg
+
+
+def update_screen():
+    pg.display.flip()
+
+
+def update_background(screen):
+    screen.fill((105, 105, 105))
+
+
+def main():
+    pg.init()
+    logo = pg.image.load("icon.png")
+    pg.display.set_icon(logo)
+    pg.display.set_caption("Game")
+    screen = pg.display.set_mode((500, 500))
+    running = True
+    update_background(screen)
+    update_screen()
+
+    # Main Loop
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                ' Put Actual Playing Code Below '
+                if pg.key.get_pressed()[pg.K_ESCAPE]:  # Simple Player Exit
+                    running = False
+            if event.type == pg.QUIT:
+                running = False
+
+
+if __name__ == "__main__":
+    main()
